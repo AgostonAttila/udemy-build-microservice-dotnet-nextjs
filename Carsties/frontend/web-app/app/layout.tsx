6a5d0bdbcf82a329cb52eb04e02780bc6e17a792
Atesz014 <atesz014@gmail.com>
2023-10-18 14:30:32 +0200

    before zustand nextjs

--- Carsties/frontend/web-app/app/layout.tsx
+++ Carsties/frontend/web-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './nav/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en">     
+      <body>
+      <Navbar/>
+      <main className='container mx-auto px-5 pt'>
+      {children}
+      </main>
+      </body>
     </html>
   )
 }
--- Carsties/frontend/web-app/app/page.tsx
+++ Carsties/frontend/web-app/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Listings from './auctions/Listings'
 
 export default function Home() {
   return (
    <div>
-    <h3 className='text-3xl font-semibold'> Carsties app!</h3>
+   <Listings/>
    </div>
    
   )
